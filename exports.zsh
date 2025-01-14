0710b8a42d03538c6bdf7cc0a59bfea557f2990a
José Maia <jose.maia@ctw.bmwgroup.com>
2025-01-14 16:23:22 +0000

    Sync changes

--- exports.zsh
+++ exports.zsh
@@ -1,7 +1,10 @@
+#configs
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export EDITOR=nano
 export VAGRANT_ALIAS_FILE=~/.dotfiles/files/vagrant-aliases
+export PASSWORD_STORE_GPG_OPTS="--armor"
+
 #personal info
 export AD_USER='ctw00143'
